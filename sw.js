e21b71cdbcc93424b792e3fd6171cb4ac4caedfa
dlgydnjshh-dotcom <dlgydnjshh@icloud.com>
2026-08-14 18:29:50 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-const C='password-shared-v3';
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest'])))});
-self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k))))])));
-self.addEventListener('fetch',e=>{
-  const u=new URL(e.request.url);
-  if(u.pathname.startsWith('/.netlify/functions/')) return;
-  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(C).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)));
-});
